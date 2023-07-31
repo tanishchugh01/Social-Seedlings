4fd4d94a688533d6b529041a672339e5b7483e59
tanishchugh01 <tanishchugh01@gmail.com>
2023-07-31 18:51:08 +0530

    feat/Added whatsapp share option

--- src/pages/post/[postId]/index.tsx
+++ src/pages/post/[postId]/index.tsx
@@ -4,23 +4,38 @@ import GroupOfPost from "@/modules/NewsFeed/GroupOfPost";
 import Profile from "@modules/Profile";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import styles from "@styles/Profile.module.css";
 import { useEffect, useState } from "react";
 
 const Page: NextPage = () => {
   const router = useRouter();
-  
-  const { postId } = router.query ;
 
-  const formatterFunction = (data:any)  => {
+  const { postId } = router.query;
+
+  const formatterFunction = (data: any) => {
     return [data];
-  }
-  
-  const urlString = "/photos/"+postId;
-  if(!router.isReady) return <LoaderComponent />
-  
+  };
+
+  const urlString = "/photos/" + postId;
+  if (!router.isReady) return <LoaderComponent />;
+
   return (
     <div className="heightFull">
-      <GroupOfPost urlString={urlString} formatterFunction={formatterFunction} />
+      <GroupOfPost
+        urlString={urlString}
+        formatterFunction={formatterFunction}
+      />
+      {/* share button */}
+
+      <div className="horizontalCenter">
+        <button
+          className={styles.pf241switchView}
+          onClick={() => {
+            window.open("https://wa.me/?text=" + window.location.href);
+          }}>
+          Share <i className="bi bi-whatsapp"></i>
+        </button>
+      </div>
     </div>
   );
 };
